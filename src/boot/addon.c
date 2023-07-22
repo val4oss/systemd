d1888c5fd89d90831299d1183186dc08175eb702
Raito Bezarius <masterancpp@gmail.com>
2023-07-22 18:03:11 +0200

    boot(pe): introduce `.binrel` as PE metadata
    Following
    https://github.com/systemd/systemd/pull/28057/files#r1233760527,
    
    we realized the need for EFI metadata, as EFI device paths will not
    always carry the filename information.
    
    Here is an attempt to materialize it as a PE section `.sdmeta` to avoid
    piling up many sections which will be all aligned to full pages in
    memory and 512 bytes on disk.
    
    This is a structured data section for any PE metadata, it starts with a
    `filename` field.
    
    In a future PR, stubs, addons and systemd-boot will make use of that
    section.

--- src/boot/addon.c
+++ src/boot/addon.c
@@ -1,10 +1,16 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 
+#include "addon-util.h"
 #include "efi.h"
+#include "pe.h"
 #include "version.h"
 
 /* Magic string for recognizing our own binaries */
 DECLARE_NOALLOC_SECTION(".sdmagic", "#### LoaderInfo: systemd-addon " GIT_VERSION " ####");
+_used_ _section_(".binrel") static const struct pe_metadata metadata = {
+        .fname = ADDON_FILENAME
+};
+
 
 /* This is intended to carry data, not to be executed */
 
